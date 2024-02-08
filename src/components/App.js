688f0e1b40408a138f69451a803543c0b1d85d97
Shivam Singh gautam <105829038+Shivu150199@users.noreply.github.com>
2024-02-08 11:08:45 +0530

    added  count button

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
+import {useState} from 'react'
 
 const App = () => {
+  const [count,setCount]=useState(0)
+  const handleCLick=()=>{
+    setCount(count++)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+    <p>Button clicked {count} times</p>
+    <button onclick={handleCLick}> click me</button>
     </div>
   )
 }
